204c0f41d0c5d1c178a61d5738c573a549e52f62
David Choi <davidchoi1@cpp.edu>
2024-02-10 16:52:39 -0800

    Project push

---
+++ src/main/java/io/spring/training/corespring/personalbudgettracker/PersonalBudgetTrackerApplication.java
@@ -0,0 +1,13 @@
+package io.spring.training.corespring.personalbudgettracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBudgetTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalBudgetTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/spring/training/corespring/personalbudgettracker/PersonalBudgetTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.training.corespring.personalbudgettracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBudgetTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
